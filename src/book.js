cc6575800129d5b2d861d3e1d1e5ff3abf2c9653
Taryn Martin <taryn.martin919@gmail.com>
2020-05-07 20:48:15 -0600

    Finished book problems

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  var ghoulCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return ghoulCharacter;
+}
+
+
+function saveReview(newReview, reviews) {
+  if (reviews.includes(newReview)) {
+    return reviews;
+  } else {
+      return reviews.push(newReview)
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  }
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
