d2ad749afba5e17a92907b2f7d7a22d6e2221324
Taryn Martin <taryn.martin919@gmail.com>
2020-05-09 10:24:16 -0600

    First try at shelf test

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,35 @@
+var scifiShelf = [];
 
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    return shelf.unshift(book);
+  } else {
+    return;
+  }
+}
+
+function unshelfBook(book, shelf) {
+  return shelf.splice(1, 1);
+}
+
+function listTitles(shelf) {
+  var titles = `${shelf[0].title}, ${shelf[1].title}, ${shelf[2].title}`;
+  return titles;
+}
+
+function searchShelf(shelf, title) {
+  var duneBook = shelf[0].title;
+  var hyperionBook = shelf[1].title
+  if (duneBook === title || hyperionBook === title) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
